53cec4c46f41fdbc2d542891bc7561f8847fd1bf
Hector Yuen <hector.yuen@gmail.com>
2017-08-05 00:19:34 -0700

    more layers

--- housenumbers/process_digits.py
+++ housenumbers/process_digits.py
@@ -2,9 +2,11 @@ import h5py
 import scipy.io as sio
 import keras
 from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras import backend as K
 from keras.models import Sequential
+from keras.utils import plot_model
+
 import numpy as np
 
 def main():
@@ -23,6 +25,13 @@ def main():
     print dir(i)
     break
 
+
+def addconvblock(model, layers, filters):
+  for i in range(layers):
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(filters, (3,3), activation='relu'))
+  model.add(MaxPooling2D((2,2), strides=(2,2)))
+            
 
 def main2():
   data = sio.loadmat('train_32x32.mat')
@@ -50,23 +59,59 @@ def main2():
   print y_train.shape
   
   model = Sequential()
-  model.add(Conv2D(32, kernel_size=(3, 3),
+  model.add(Conv2D(64, kernel_size=(3, 3),
                  activation='relu',
                  input_shape=input_shape))
-  #model.add(Conv2D(64, (3, 3), activation='relu'))
+
+  addconvblock(model, 2, 64)
+  addconvblock(model, 2, 128)
+  addconvblock(model, 3, 256)
+  #addconvblock(model, 3, 512)
+  #addconvblock(model, 3, 512)
+
+
+  """
+  model.add(ZeroPadding2D((1, 1)))
+  model.add(Conv2D(64, kernel_size=(3, 3),
+                 activation='relu'))
+
+
+  model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu'))
+  model.add(Conv2D(16, kernel_size=(3, 3),
+                 activation='relu'))
+  model.add(Conv2D(8, kernel_size=(3, 3),
+                 activation='relu'))
+  #model.add(Conv2D(22, kernel_size=(3, 3),
+  #               activation='relu'))
+  #model.add(Conv2D(20, kernel_size=(3, 3),
+  #               activation='relu'))
+
+  model.add(MaxPooling2D(pool_size=(2, 2), strides=(2,2)))
+
+  
+  
+  model.add(Conv2D(15, kernel_size=(3, 3),
+                 activation='relu'))
   model.add(MaxPooling2D(pool_size=(2, 2)))
-  #model.add(Dropout(0.25))
+  """
+
   model.add(Flatten())
-  model.add(Dense(16, activation='relu'))
+  model.add(Dense(2048, activation='relu'))
   model.add(Dropout(0.5))
+  model.add(Dense(2048, activation='relu'))
+  model.add(Dropout(0.5))
+
+
+
   model.add(Dense(num_classes, activation='softmax'))
   
   model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
+  plot_model(model, to_file='model.png', show_shapes=True)
 
   model.fit(x_train, y_train,
-            batch_size=100, epochs=10000, verbose = 1, validation_split=0.1)
-  #            x_test, y_test)
+            batch_size=10, epochs=1000, verbose = 1, validation_split=0.2)
   
 main2()
